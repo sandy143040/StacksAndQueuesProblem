dec14630f5a5278b8cc557391ff29d0b5559c21c
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-05-11 23:19:16 +0530

    [Sandeep] Adding welcome message

---
+++ StacksAndQueuesProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueuesProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queues problem");
+            Console.ReadLine();
+        }
+    }
+}
